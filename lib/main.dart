dc39d7b5fd6f06b45298b781073f110942492390
ghulam-rasool-dev <ghulam.rasool.freelance@gmail.com>
2021-01-22 16:31:54 +0500

    Changed Audio Player

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,29 @@
-import 'package:audioplayers/audio_cache.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(Xylophone());
-}
+void main() => runApp(Xylophone());
 
 class Xylophone extends StatelessWidget {
+  void playsound(int n) {
+    final assetsAudioPlayer = AssetsAudioPlayer();
+
+    assetsAudioPlayer.open(
+      Audio("assets/note$n.wav"),
+    );
+  }
+  Widget buildkey({int n, Color c}) {
+    return Expanded(
+      child: Container(
+        color: c,
+        child: FlatButton(
+          onPressed: () {
+            playsound(n);
+          },
+        ),
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,91 +31,20 @@ class Xylophone extends StatelessWidget {
       title: 'Xylophone',
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.blueAccent,
-          title: Center(child: Text('Xylophone'))
-        ),
+            backgroundColor: Colors.blueAccent,
+            title: Center(child: Text('Xylophone'))),
         body: SafeArea(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              Expanded(
-                child: Container(
-                  color: Colors.blue.shade900,
-                  child: FlatButton(
-                    onPressed: () {
-                      final Player = AudioCache();
-                      Player.play('note1.wav');
-                    },
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.red,
-                  child: FlatButton(
-                    onPressed: () {
-                      final Player = AudioCache();
-                      Player.play('note2.wav');
-                    },
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.green,
-                  child: FlatButton(
-                    onPressed: () {
-                      final Player = AudioCache();
-                      Player.play('note3.wav');
-                    },
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.orange,
-                  child: FlatButton(
-                    onPressed: () {
-                      final Player = AudioCache();
-                      Player.play('note4.wav');
-                    },
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.purple,
-                  child: FlatButton(
-                    onPressed: () {
-                      final Player = AudioCache();
-                      Player.play('note5.wav');
-                    },
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.yellow,
-                  child: FlatButton(
-                    onPressed: () {
-                      final Player = AudioCache();
-                      Player.play('note6.wav');
-                    },
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.teal,
-                  child: FlatButton(
-                    onPressed: () {
-                      final Player = AudioCache();
-                      Player.play('note7.wav');
-                    },
-                  ),
-                ),
-              ),
+              buildkey(n: 1, c: Colors.purple),
+              buildkey(n: 2, c: Colors.red),
+              buildkey(n: 3, c: Colors.teal),
+              buildkey(n: 4, c: Colors.amber),
+              buildkey(n: 5, c: Colors.brown),
+              buildkey(n: 6, c: Colors.cyan),
+              buildkey(n: 7, c: Colors.green),
             ],
           ),
         ),
